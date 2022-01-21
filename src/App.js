bf5aa2d3419b0fa059c065427d0021dd106c1363
del <rodelcdavid@gmail.com>
2022-01-21 08:10:25 +0800

    initialized project and its components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import FilterMenu from "./components/FilterMenu";
+import Results from "./components/Results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>PH Medical Specialist Finder</h1>
+      <FilterMenu />
+      <Results />
+    </>
   );
 }
 
---
+++ src/components/FilterMenu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilterMenu = () => {
+  return <div>Filters</div>;
+};
+
+export default FilterMenu;
