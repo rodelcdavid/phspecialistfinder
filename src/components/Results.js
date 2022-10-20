ecaeacc30754b63787ea41a15cffe039c462a6a8
del <rodelcdavid@gmail.com>
2022-10-20 10:16:29 +0800

    - simplified row data - improved variable names

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,50 +4,47 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import React, { useContext, useEffect, useState } from "react";
-import { Box, Button } from "@mui/material";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
 import { MatchContext } from "../context/MatchContext";
+import { FilterContext } from "../context/FilterContext";
+import { data } from "../data";
 
-const Results = ({ data }) => {
+const Results = () => {
   const { match } = useContext(MatchContext);
+  const { specialty, municipality, province } = useContext(FilterContext);
 
+  const tableRef = useRef();
+
+  //Load more results
   const [visible, setVisible] = useState(match.length > 20 ? 20 : match.length);
-  const displayMatch = match.slice(0, visible);
-
-  const handleGoogleSearch = (fullname) => {
-    window.open("http://google.com/search?q=" + fullname);
-  };
-
+  const visibleMatch = match.slice(0, visible);
   const handleLoadMore = () => {
     match.length - visible >= 20
       ? setVisible((prev) => prev + 20)
       : setVisible(match.length);
   };
 
-  const rowData = displayMatch.map((entry) => {
-    let id, fullname, specialty, municipality, province;
-    if (entry.length === 9) {
-      id = entry[0];
-      fullname = `${entry[1]} ${entry[2]} ${entry[3]}`;
-      specialty = entry[4];
-      municipality = entry[5];
-      province = entry[6];
-    } else if (entry.length === 10) {
-      id = entry[0];
-      fullname = `${entry[1]} ${entry[2]} ${entry[3]} ${entry[4]}`;
-      specialty = entry[5];
-      municipality = entry[6];
-      province = entry[7];
-    } else {
-      id = entry[0];
-      fullname = `${entry[1]} ${entry[2]} ${entry[3]} ${entry[4]} ${entry[5]}`;
-      specialty = entry[6];
-      municipality = entry[7];
-      province = entry[8];
-    }
+  const rowDisplay = visibleMatch.map((entry) => {
+    const id = entry[0];
+    const fullname = `${entry[1]} ${entry[2]} ${entry[3]}`;
+    const specialty = entry[4];
+    const municipality = entry[5];
+    const province = entry[6];
+
     return { id, fullname, specialty, municipality, province };
   });
+
+  const handleGoogleSearch = (fullname) => {
+    window.open("http://google.com/search?q=" + fullname);
+  };
+
+  //Table scrolls to top when filter changes
+  useEffect(() => {
+    setTimeout(() => {
+      tableRef.current?.scrollTo(0, 0);
+    }, 200);
+  }, [specialty, municipality, province]);
 
   useEffect(() => {
     match.length > 20 ? setVisible(20) : setVisible(match.length);
@@ -68,23 +65,22 @@ const Results = ({ data }) => {
             color: "#444",
           }}
         >
-          <p>
-            <em>Nothing to show.</em>
-          </p>
-          <p>
-            <em>Please set search criteria first</em>
-          </p>
+          <Typography
+            sx={{ fontStyle: "italic", textAlign: "center" }}
+            variant="body1"
+          >
+            Nothing to show.
+            <br />
+            Please set search criteria first.
+          </Typography>
         </Box>
       ) : (
         <>
           <TableContainer
-            // component={Paper}
             sx={{
               paddingBottom: "0.5rem",
               border: "1px solid #aaa",
-              // width: "90vw",
               margin: "1rem auto",
-              // boxShadow: 2,
               overflow: "auto",
               height: "75%",
 
@@ -99,11 +95,11 @@ const Results = ({ data }) => {
                 background: "#888",
               },
             }}
+            ref={tableRef}
           >
             <Table
               sx={{
                 minWidth: 650,
-                // boxShadow: "0 0 10px rgba(0,0,0,0.23)",
 
                 "& > thead > tr > th": {
                   fontWeight: 800,
@@ -129,11 +125,10 @@ const Results = ({ data }) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rowData.map((entry) => (
+                {rowDisplay.map((entry) => (
                   <TableRow
                     key={entry.id}
                     onClick={() => handleGoogleSearch(entry.fullname)}
-                    // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
                     <TableCell>{entry.id}</TableCell>
                     <TableCell>{entry.fullname}</TableCell>
@@ -144,21 +139,24 @@ const Results = ({ data }) => {
                 ))}
               </TableBody>
             </Table>
-            <div
-              style={{
+            <Box
+              sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
+                height: "75%",
+                color: "#444",
               }}
             >
-              <p style={{ padding: "1rem" }}>
-                <em>
-                  {match.length !== 0
-                    ? `- Showing ${visible} out of ${match.length} results -`
-                    : "No results found."}
-                </em>
-              </p>
+              <Typography
+                sx={{ fontStyle: "italic", padding: "1rem" }}
+                variant="body1"
+              >
+                {match.length !== 0
+                  ? `- Showing ${visible} out of ${match.length} results -`
+                  : "No results found."}
+              </Typography>
               {match.length > 20 && visible !== match.length && (
                 <Button
                   variant="contained"
@@ -171,102 +169,12 @@ const Results = ({ data }) => {
                   Load more
                 </Button>
               )}
-            </div>
+            </Box>
           </TableContainer>
         </>
       )}
     </>
   );
-
-  //   return (
-  //     <>
-  //       {/* <p style={{ textAlign: "center" }}>
-  //         {match.length === data.length ? "-" : `Found: ${match.length}`}
-  //       </p> */}
-  //       <Box
-  //         sx={{
-  //           padding: "0 1rem",
-  //           margin: "1rem auto",
-  //           width: "100%",
-  //           overflowX: "auto",
-  //           border: "2px solid #000",
-  //         }}
-  //       >
-  //         <div
-  //           style={{
-  //             display: "flex",
-  //             fontWeight: "bolder",
-  //             borderBottom: "1px solid black",
-  //           }}
-  //         >
-  //           <span style={{ width: "80px" }}> </span>
-  //           <span style={{ width: "400px" }}>FULL NAME</span>
-  //           <span style={{ width: "300px" }}>SPECIALTY</span>
-  //           <span style={{ width: "200px" }}>MUNICIPALITY</span>
-  //           <span style={{ width: "200px" }}>PROVINCE</span>
-  //         </div>
-  //         {match.length === data.length ? (
-  //           <p style={{ textAlign: "center" }}>
-  //             <em>Please set search criteria first.</em>
-  //           </p>
-  //         ) : (
-  //           <>
-  //             {rowData.map((entry, i) => {
-  //               return (
-  //                 //CHange this to table
-
-  //                 <Box
-  //                   sx={{
-  //                     display: "flex",
-  //                     cursor: "pointer",
-  //                     transition: "all 150ms ease-in",
-  //                     "&:hover": {
-  //                       backgroundColor: "rgba(0,0,0,0.2)",
-  //                       fontWeight: "bolder",
-  //                     },
-  //                   }}
-  //                   key={i}
-  //                   onClick={() => handleGoogleSearch(entry.fullname)}
-  //                 >
-  //                   <span style={{ width: "80px" }}>{entry.id}</span>
-  //                   <span style={{ width: "400px" }}>{entry.fullname}</span>
-  //                   <span style={{ width: "300px" }}>{entry.specialty} </span>
-  //                   <span style={{ width: "200px" }}>{entry.municipality} </span>
-  //                   <span style={{ width: "200px" }}>{entry.province} </span>
-  //                 </Box>
-  //               );
-  //             })}
-  //             <div
-  //               style={{
-  //                 display: "flex",
-  //                 flexDirection: "column",
-  //                 alignItems: "center",
-  //               }}
-  //             >
-  //               <p>
-  //                 <em>
-  //                   - Showing {visible} out of {match.length} -
-  //                 </em>
-  //               </p>
-  //               {match.length > 20 && visible != match.length && (
-  //                 <button
-  //                   style={{
-  //                     width: "200px",
-  //                     margin: "0 auto",
-  //                     padding: "0.5rem",
-  //                     cursor: "pointer",
-  //                   }}
-  //                   onClick={handleLoadMore}
-  //                 >
-  //                   Load more
-  //                 </button>
-  //               )}
-  //             </div>
-  //           </>
-  //         )}
-  //       </Box>
-  //     </>
-  //   );
 };
 
 export default Results;
