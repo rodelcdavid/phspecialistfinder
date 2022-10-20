61a21142ef3f34c0435bd9bd94d0221574835d4a
del <rodelcdavid@gmail.com>
2022-10-20 10:09:23 +0800

    - removed loading state - removed prop drilling - fixed heading and box size for smaller screen size

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { useMediaQuery } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
 import FilterMenu from "./components/FilterMenu";
 import FilterMenuMobile from "./components/FilterMenuMobile";
 import Results from "./components/Results";
 import { FilterProvider } from "./context/FilterContext";
 import { MatchProvider } from "./context/MatchContext";
-import { data } from "./data";
 import globalStyle from "./globalStyle";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
   const large = useMediaQuery("(min-width:1000px)");
   return (
     <MatchProvider>
@@ -24,32 +20,33 @@ function App() {
           borderRadius: "10px",
           boxShadow: "0 5px 5px rgba(0,0,0,0.23)",
           padding: "1rem",
-          // minHeight: "calc(100vh - 4rem)",
-          width: "100%",
           height: "calc(100vh - 2rem)",
+          minHeight: "550px",
+          overflow: "auto",
         }}
       >
-        <h1
-          style={{
+        <Typography
+          variant="h5"
+          sx={{
             textAlign: "center",
             marginTop: "0.5rem",
             color: "#1769aa",
-            fontSize: "1.5rem",
+            fontWeight: 600,
+            fontSize: "1.2rem",
+
+            "@media (min-width: 450px)": {
+              fontSize: "1.5rem",
+            },
           }}
         >
           PH Medical Specialist Finder
-        </h1>
+        </Typography>
         <FilterProvider>
-          {large ? (
-            <FilterMenu data={data} />
-          ) : (
-            <FilterMenuMobile data={data} />
-          )}
-        </FilterProvider>
+          {large ? <FilterMenu /> : <FilterMenuMobile />}
 
-        <Results data={data} />
+          <Results />
+        </FilterProvider>
       </Box>
-      {/* <Footer /> */}
     </MatchProvider>
   );
 }
